4eeb82551cc7d0d9386e10c57b4fb19a3c339fa3
anoff <offenhaeuser@gmail.com>
2018-03-13 21:00:38 +0100

    :star2: feat: local runscript in docker container for classification

--- app/breed_classifier.py
+++ app/breed_classifier.py
@@ -1,4 +1,7 @@
 from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential
 from sklearn.datasets import load_files       
 from keras.utils import np_utils
 import numpy as np
@@ -7,12 +10,22 @@ from util import path_to_tensor
 
 dog_names = ['Affenpinscher', 'Afghan_hound', 'Airedale_terrier', 'Akita', 'Alaskan_malamute', 'American_eskimo_dog', 'American_foxhound', 'American_staffordshire_terrier', 'American_water_spaniel', 'Anatolian_shepherd_dog', 'Australian_cattle_dog', 'Australian_shepherd', 'Australian_terrier', 'Basenji', 'Basset_hound', 'Beagle', 'Bearded_collie', 'Beauceron', 'Bedlington_terrier', 'Belgian_malinois', 'Belgian_sheepdog', 'Belgian_tervuren', 'Bernese_mountain_dog', 'Bichon_frise', 'Black_and_tan_coonhound', 'Black_russian_terrier', 'Bloodhound', 'Bluetick_coonhound', 'Border_collie', 'Border_terrier', 'Borzoi', 'Boston_terrier', 'Bouvier_des_flandres', 'Boxer', 'Boykin_spaniel', 'Briard', 'Brittany', 'Brussels_griffon', 'Bull_terrier', 'Bulldog', 'Bullmastiff', 'Cairn_terrier', 'Canaan_dog', 'Cane_corso', 'Cardigan_welsh_corgi', 'Cavalier_king_charles_spaniel', 'Chesapeake_bay_retriever', 'Chihuahua', 'Chinese_crested', 'Chinese_shar-pei', 'Chow_chow', 'Clumber_spaniel', 'Cocker_spaniel', 'Collie', 'Curly-coated_retriever', 'Dachshund', 'Dalmatian', 'Dandie_dinmont_terrier', 'Doberman_pinscher', 'Dogue_de_bordeaux', 'English_cocker_spaniel', 'English_setter', 'English_springer_spaniel', 'English_toy_spaniel', 'Entlebucher_mountain_dog', 'Field_spaniel', 'Finnish_spitz', 'Flat-coated_retriever', 'French_bulldog', 'German_pinscher', 'German_shepherd_dog', 'German_shorthaired_pointer', 'German_wirehaired_pointer', 'Giant_schnauzer', 'Glen_of_imaal_terrier', 'Golden_retriever', 'Gordon_setter', 'Great_dane', 'Great_pyrenees', 'Greater_swiss_mountain_dog', 'Greyhound', 'Havanese', 'Ibizan_hound', 'Icelandic_sheepdog', 'Irish_red_and_white_setter', 'Irish_setter', 'Irish_terrier', 'Irish_water_spaniel', 'Irish_wolfhound', 'Italian_greyhound', 'Japanese_chin', 'Keeshond', 'Kerry_blue_terrier', 'Komondor', 'Kuvasz', 'Labrador_retriever', 'Lakeland_terrier', 'Leonberger', 'Lhasa_apso', 'Lowchen', 'Maltese', 'Manchester_terrier', 'Mastiff', 'Miniature_schnauzer', 'Neapolitan_mastiff', 'Newfoundland', 'Norfolk_terrier', 'Norwegian_buhund', 'Norwegian_elkhound', 'Norwegian_lundehund', 'Norwich_terrier', 'Nova_scotia_duck_tolling_retriever', 'Old_english_sheepdog', 'Otterhound', 'Papillon', 'Parson_russell_terrier', 'Pekingese', 'Pembroke_welsh_corgi', 'Petit_basset_griffon_vendeen', 'Pharaoh_hound', 'Plott', 'Pointer', 'Pomeranian', 'Poodle', 'Portuguese_water_dog', 'Saint_bernard', 'Silky_terrier', 'Smooth_fox_terrier', 'Tibetan_mastiff', 'Welsh_springer_spaniel', 'Wirehaired_pointing_griffon', 'Xoloitzcuintli', 'Yorkshire_terrier']
 
+def build_model():
+    Resnet50_model = Sequential()
+    Resnet50_model.add(Flatten(input_shape=(1, 1, 2048)))
+    Resnet50_model.add(Dense(200, activation='relu'))
+    Resnet50_model.add(Dropout(0.3))
+    Resnet50_model.add(Dense(133, activation='softmax'))
+    Resnet50_model.load_weights('assets/weights.best.Resnet50.hdf5')
+    return Resnet50_model
+
 def extract_Resnet50(tensor):
 	return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
 
+Resnet50_model = build_model()
 # return the dog breed from path to an image as input
 def dog_breed(img_path):
-	ResNet50_model = ResNet50(weights='imagenet')
-	features = extract_Resnet50(path_to_tensor(img_path))
-	prediction = ResNet50_model.predict(features)
-	return dog_names[np.argmax(prediction)]
+    features = extract_Resnet50(path_to_tensor(img_path))
+    prediction = Resnet50_model.predict(features)
+    print(prediction)
+    return dog_names[np.argmax(prediction)]
---
+++ app/run.py
@@ -0,0 +1,56 @@
+import cv2
+from time import time
+from io import BytesIO
+from flask import Flask, send_file, render_template
+from breed_classifier import dog_breed
+from species_detector import dog_detector, face_detector
+import matplotlib.pyplot as plt  
+
+# gimme images of stuff!
+def lol_you_look_like_a_dog(img_path):
+    is_dog = True#dog_detector(img_path)
+    is_human = False#face_detector(img_path)
+    breed = dog_breed(img_path)
+    breed = "test"
+    is_error = False
+    message = ""
+    if is_dog and is_human:
+        message = "Something went wrong you're both human and dog!"
+        is_error = True
+    elif not is_dog and not is_human:
+        message = "You're neither human nor dog..what are you?"
+        is_error = True
+    elif is_human:
+        message = "Hey human 😊, you look like a {}".format(breed)
+    else: # this should only match is_dog=True is_human=False
+        message = "Y0 dawg 🐶, who's a good {}".format(breed)
+    
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    figure = plt.figure()
+    plt.imshow(img)
+    plt.text(0, -10, message, fontsize=12, color=('r' if is_error else 'b'))
+    plt.axis('off')
+    plt.show()
+    return figure
+
+def init():
+    start = time()
+    print("initing..")
+    from keras.applications.resnet50 import ResNet50
+    ResNet50(weights='imagenet', include_top=False)
+    print("init done in {}s".format(time() - start))
+
+def run():
+    start = time()
+    print("running..")
+    fig = lol_you_look_like_a_dog("assets/anoff.jpg")
+    img = BytesIO()
+    fig.savefig(img)
+    img.seek(0)
+    print("run done in {}s".format(time() - start))
+
+if __name__ == '__main__':
+    init()
+    run()
+    print("yay")
